831394f265fba303e70bb6b90a3a128920e61b90
Ethan-Newman <79428697+Ethan-Newman@users.noreply.github.com>
2021-10-07 08:36:21 -0400

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-# Author: Ethan Newman
-# Description: Main program for running Hangman, utilizes RandomWord, Hangman, and GUI classes.
-
-from GUI import GUI
-
-if __name__ == '__main__':
-
-   gui = GUI()
-   gui.start_up()
